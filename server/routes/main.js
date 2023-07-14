62b00428a8efe4467401f09901ad1e81d736679e
mgzawlinnaing <alexandermike1999@gmail.com>
2023-07-14 23:44:45 +0630

    feat: add function to routes, create view-pages, layouts and partials

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Routes
 router.get("", (req, res) => {
-  res.send("hello, world!");
+  //   res.send("hello, world!");
+  const locals = {
+    title: "Zaw Lin's Blog",
+    description: "My simple blog created w/ node, express & mongodb.",
+  };
+
+  res.render("index", { locals });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
